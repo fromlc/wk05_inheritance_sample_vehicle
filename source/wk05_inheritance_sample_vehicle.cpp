ad61e4d930e933e8bd72e9a26d646b6fb6f934c8
L Carver <wifihum@gmail.com>
2023-03-02 15:34:24 -0800

    experiment with typeid and decltype inline and in function call

--- source/wk05_inheritance_sample_vehicle.cpp
+++ source/wk05_inheritance_sample_vehicle.cpp
@@ -23,7 +23,18 @@
 //------------------------------------------------------------------------------
 using std::cin;
 using std::cout;
+using std::string;
 using std::vector;
+
+//------------------------------------------------------------------------------
+// globals
+//------------------------------------------------------------------------------
+//namespace poly { vector<Vehicle*> vv; }
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+inline void showInfo(vector<Vehicle*>&);
 
 //------------------------------------------------------------------------------
 // entry point
@@ -34,34 +45,76 @@ int main() {
     //      2) Car instances (Car is-a Vehicle), and
     //      3) Boat instances (Boat is-a Vehicle)
 
-    // create a Car instance and store its address in our vector
     vector<Vehicle*> vv;
 
-    Vehicle* pVehicle = new Vehicle;
-    vv.push_back(pVehicle);
-    cout << "Element " << vv.size() - 1 
-        << " is a " << pVehicle->getInfo() << "\n\n";
+    // vector of Vehicle* can hold pointers to any class derived from Vehicle
+    Vehicle* pV = new Vehicle;
+    vv.push_back(pV);
+    cout << "added Vehicle element\n\n";
+    decltype(pV) pDeclV = pV;
+    cout << "decltype " << typeid(pDeclV).name() << '\n';
 
-    Car* pCar = new Car;
-    vv.push_back(pCar);
-    cout << "Element " << vv.size() - 1
-        << " is a " << pCar->getInfo() << "\n\n";
+    cout << typeid(pV).name() << '\n';
+    cout << pV->getInfo() << ' ' << pV->getRegType() << "\n\n";
 
-    Boat* pBoat = new Boat;
-    vv.push_back(pBoat);
-    cout << "Element " << vv.size() - 1
-        << " is a " << pBoat->getInfo() << "\n\n";
+    Car* pC = new Car;
+    vv.push_back(pC);
+    cout << "added Car element\n\n";
 
-    cout << "Traversing vector vv:\n\n";
+    decltype(pC) pDeclC = pC;
+    cout << "decltype " << typeid(pDeclC).name() << '\n';
+
+    cout << typeid(pC).name() << '\n';
+    cout << pC->getInfo() << ' ' << pC->getRegType() << "\n\n";
+
+    Boat* pB = new Boat;
+    vv.push_back(pB);
+    cout << "added Boat element\n\n";
+
+    decltype(pB) pDeclB = pB;
+    cout << "decltype " << typeid(pDeclB).name() << '\n';
+
+    cout << typeid(pB).name() << '\n';
+    cout << pB->getInfo() << ' ' << pB->getRegType() << "\n\n";
 
     // each vector element displays its info based on its class
-    for (Vehicle* pV : vv) {
-        cout << pV->getInfo() << ' '
-            << pV->getRegType() << '\n';
+    
+    // with code in the same function
+    cout << "main(): traversing vector of typeid "
+        << typeid(vv).name() << ":\n\n";
+
+    for (int i = 0; i < vv.size(); ++i) {
+        decltype(vv.at(i)) pDecl = vv.at(i);
+        cout << "decltype " << typeid(pDecl).name() << '\n';
+
+        auto p = vv.at(i);
+        cout << typeid(p).name() << '\n';
+        cout << p->getInfo() << ' ' << p->getRegType() << "\n\n";
     }
+
+    // with code in a called function
+    showInfo(vv);
 
     cout << '\n';
     cin.get();
 
     return 0;
+}
+
+//------------------------------------------------------------------------------
+// displays info and vector index for any vector element type
+//------------------------------------------------------------------------------
+void showInfo(vector<Vehicle*>& v) {
+
+    cout << "showInfo(): traversing vector of typeid " 
+        << typeid(v).name() << ":\n\n";
+
+    for (int i = 0; i < v.size(); ++i) {
+        decltype(v.at(i)) pDecl = v.at(i);
+        cout << "decltype " << typeid(pDecl).name() << '\n';
+
+        auto p = v.at(i);
+        cout << typeid(p).name() << '\n';
+        cout << p->getInfo() << ' ' << p->getRegType() << "\n\n";
+    }
 }
